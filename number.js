83c7a0c6bb44616ff485769b05f0cd44f2d1361b
bhlezzie <garbinbhlezzie@gmail.com>
2024-09-10 13:01:15 +0800

    Create number.js

---
+++ number.js
@@ -0,0 +1,29 @@
+// Create a variable named firstFavNum and store your favorite number
+let firstFavNum = 11;
+
+//Create a variable named secondFavNum and store your second favorite number
+let secondFavNum = 6;
+
+// Addition
+let addition = firstFavNum + secondFavNum;
+console.log("Adition: " + addition);
+
+//Subtraction
+let subtraction = firstFavNum - secondFavNum;
+console.log("Subtraction: " + subtraction);
+
+//Multiplication
+let multiplication = firstFavNum / secondFavNum;
+console.log("Multiplication: " + multiplication);
+
+//Division
+let division = firstFavNum / secondFavNum;
+console.log("Division: " + division);
+
+//Modudlus
+let modulus = firstFavNum % secondFavNum;
+console.log("Modulus: " + modulus);
+
+//Exponentiation (Power)
+let exponentiation = firstFavNum ** secondFavNum;
+console.log("Exponentiation: " + exponentiation);
